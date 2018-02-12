276e3bd217ea8c4c7c362ed7799f98f3bbea3bb8
Maxym Sahaydak <maxym.sahaydak@indeema.com>
2018-02-12 23:28:38 +0200

    - Added Upcoming Launches view controller; - Added Launch base info table view cell;

---
+++ Rocket Launch/Rocket Launch/Presentation/UpcomingLaunches/UpcomingLaunchesVC.swift
@@ -0,0 +1,35 @@
+//
+//  UpcomingLaunchesViewController.swift
+//  Rocket Launch
+//
+//  Created by Maxym Sahaydak on 2/12/18.
+//  Copyright © 2018 Maxym Sahaydak. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingLaunchesVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
